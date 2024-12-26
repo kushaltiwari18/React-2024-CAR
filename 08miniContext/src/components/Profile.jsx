ec23933f54192e92b82604411890559dcefb3d7c
kushaltiwari18 <hotminzz16@gmail.com>
2024-12-26 22:15:15 +0530

    arrow function Add

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React,{useContext} from 'react'
 import UserContext from '../context/UserContext'
 
 
-function Profile() {
+const Profile = ()=> {
     const {user} = useContext(UserContext);
 
     if(!user) return <div>Please login</div>
